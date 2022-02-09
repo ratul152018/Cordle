8648e36008c9104310e851e804e40455fcc030b0
Md. Raisul Alam Ratul <68326674+ratul152018@users.noreply.github.com>
2022-02-09 14:14:25 +0600

    Update Cordle.py

--- Cordle.py
+++ Cordle.py
@@ -8,7 +8,7 @@ def make_raw(str):
     return str
 
 
-with open("words.txt", 'r') as f:
+with open(f"{os.path.dirname(__file__)}/words.txt", 'r') as f:
     word_list = f.readlines()
     
 random = random.randint(0,len(word_list))
@@ -30,7 +30,7 @@ Guess the CORDLE in 6 tries.
     Examples:
         Draft
         DR^--
-
+/
     The letter D and R is in the word and in the correct spot.
     The letter A is in the word but in the wrong spot.
     The letter U is not in the word in any spot.
